162768e9572ec1959bbb02d9a6aaf00b5b54f610
IAmNotAltsRULE <86839673+IAmNotAltsRULE@users.noreply.github.com>
2025-06-12 11:15:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+require('dotenv').config();
+const express = require('express');
+const { createClient } = require('@supabase/supabase-js');
+const path = require('path');
+const app = express();
+
 const express = require('express');
 const path = require('path');
 const app = express();
